d03f070d37fca0949f166308593bdf09636ed5b3
harrycoulton <hfcoulton93@gmail.com>
2019-10-28 23:20:45 +0000

    fixed the issue of the timeout only being enacted once, by nesting the timeout and runsearch functions into another functions which will iteratively search and destroy results until it finds a result which plays within 4.5 seconds of hitting play.

--- app/js/dist/domhandling.js
+++ app/js/dist/domhandling.js
@@ -1,12 +1,15 @@
 document.querySelector('.findRadio').addEventListener('submit', function(e){
     e.preventDefault();
+    searchDestroy()
+})
+
+function searchDestroy() {
     runSearch(document.querySelector('.searchTerm').value)
     setTimeout(function(){
         var myAudio = document.querySelector('audio')
         if (myAudio.duration > 0 && !myAudio.paused) {
-            
         } else {
-            runSearch(document.querySelector('.searchTerm').value)
+            searchDestroy()
         } 
     }, 4500)
-})
+    }
